2ab0914ae8edae8617c01d0513c589bf60b5a982
Cesar Arturo Almazan Peña <146864350+CesarAlmazan@users.noreply.github.com>
2024-03-12 16:59:35 +0000

    12/03/24

--- miPrincipal/ClienteMain.java
+++ miPrincipal/ClienteMain.java
@@ -8,6 +8,9 @@ public class ClienteMain{
         pila.apilar(2);
         pila.apilar(5);
         pila.apilar(7);
-
+        System.out.println("el tope dela pila es "+ pila.cima());
+        //se desapila
+        pila.retirar();
+        System.out.println("el tope dela pila es "+ pila.cima());
         }
 }
